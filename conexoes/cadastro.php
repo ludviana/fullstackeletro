7b3e565a87b2b958de574f2c48e40a0848ff052d
Lucas Damasceno <lucasd.viana1993@gmail.com>
2020-10-30 23:24:02 -0300

    inserindo script do banco de dados

--- conexoes/cadastro.php
+++ conexoes/cadastro.php
@@ -15,10 +15,35 @@
         $resultado = mysqli_query($conexao,$sqlIn);
 
         if($resultado){
-            echo "<p>Compra efetuada com sucesso </p>
-                <p>Obrigado por comprar conosco ".$nome."</p>
-                <p>Seu pedido será entregue em alguns dias no endereço: </p>".$endereco;
             header("refresh:5 ; ../html/produtos.php");
+            ?>
+
+            <!DOCTYPE html>
+            <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <style>
+                    body{
+                        text-align:center;
+                    }
+                    p{
+                        font-size:20pt;
+                        color: red;
+                        margin-top:110px;
+                    }
+                </style>
+
+                <title>Obrigado pela compra</title>
+            </head>
+            <body>
+                <p>Compra efetuada com sucesso </p>
+                <p>Obrigado por comprar conosco <?php echo $nome?></p>
+                <p>Seu pedido será entregue em 7 dias no endereço: <?php echo $endereco;?> </p>                
+            </body>
+            </html>
+             
+            
+            <?php
         } else{
             echo "Falha no pedido";
         }
---
+++ scriptfullstack.sql
@@ -0,0 +1,40 @@
+create database if not exists lojarecode;
+use lojarecode;
+
+CREATE TABLE `lojarecode`.`pedidos` (
+  `idpedido` INT NOT NULL AUTO_INCREMENT,
+  `nomeCliente` VARCHAR(200) NULL,
+  `endereco` VARCHAR(200) NULL,
+  `telefone` INT NULL,
+  `nomeProduto` VARCHAR(100) NULL,
+  `valorUnitario` DECIMAL(8,2) NULL,
+  `quantidade` VARCHAR(45) NULL,
+  PRIMARY KEY (`idpedido`));
+  
+  
+CREATE TABLE `lojarecode`.`produtos` (
+  `id_produto` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NULL,
+  `descricao` VARCHAR(300) NULL,
+  `preco_antigo` DECIMAL(8,2) NULL,
+  `preco` DECIMAL(8,2) NULL,
+  `imagem` VARCHAR(300) NULL,
+  PRIMARY KEY (`id_produto`));
+  
+insert into produtos  values   
+(null, 'geladeira', 'Geladeira', '2000.00', '1500.00', '../fotos/geladeira2.jpg'),
+(null, 'geladeira', 'Geladeira', '6389.00', '5019.00', '../fotos/geladeira.jpg'),
+(null, 'geladeira', 'Geladeira', '3500.00', '2000.00', '../fotos/geladeira3.jpg'),
+(null, 'fogao', 'Fogão', '1000.00', '919.00', '../fotos/fogao.jpg'),
+(null, 'fogao', 'Fogão', '1856.00', '1239.00', '../fotos/fogao2.jpg'),
+(null, 'microondas', 'Microondas', '800.00', '780.00', '../fotos/micro.jpg'),
+(null, 'microondas', 'Microondas', '900.00', '832.00', '../fotos/micro2.jpg'),
+(null, 'microondas', 'Microondas', '600.00', '498.00', '../fotos/micro3.jpg'),
+(null, 'lavaropas', 'Maquina de lavar roupas', '4725.00', '4145.00', '../fotos/maquina.jpg'),
+(null, 'lavaropas', 'Maquina de lavar roupas', '3000.00', '2754.00', '../fotos/maquinq2.jpg'),
+(null, 'lavalocas', 'Maquina de lavar louças', '4000.00', '3895.00', '../fotos/lavalouca2.jpg'),
+(null, 'lavalocas', 'Maquina de lavar louças', '5775.00', '4968.00', '../fotos/lavalouca3.jpg');
+
+  
+  
+  
