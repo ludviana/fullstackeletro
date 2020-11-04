e68961c211822bedb0a5ddf846687ff0439cbc61
Lucas Damasceno <lucasd.viana1993@gmail.com>
2020-11-04 20:09:58 -0300

    alteraçoes de bootstrap.

--- includes/menu.php
+++ includes/menu.php
@@ -1,8 +1,8 @@
-<div class="container-fluid text-white" style="background-color: red;">
+<div class="container-fluid" style="background-color: red;">
     <nav class="nav nav-justified justify-content-center align-items-center">
-        <a class="navbar-brand nav-link" href="../html/index.php"> <img width="100px" src="../fotos/asd.png" alt="Logo Fullstack"></a>
-        <a class="navbar-brand nav-link" href="../html/produtos.php">Produtos</a>
-        <a class="navbar-brand nav-link" href="../html/lojas.php">Nossas lojas</a>
-        <a class="navbar-brand nav-link" href="../html/contato.php">Fale conosco</a>
+        <a class="navbar-brand nav-link text-white" href="../html/index.php"> <img width="100px" src="../fotos/asd.png" alt="Logo Fullstack"></a>
+        <a class="navbar-brand nav-link text-white" href="../html/produtos.php">Produtos</a>
+        <a class="navbar-brand nav-link text-white" href="../html/lojas.php">Nossas lojas</a>
+        <a class="navbar-brand nav-link text-white" href="../html/contato.php">Fale conosco</a>
     </nav>
 </div>
